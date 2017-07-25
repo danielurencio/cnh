5a202b75471e99f221f3d00a739bebe0d811c147
danielurencio <danielurencio@gmail.com>
2017-07-24 20:29:12 -0500

    avance

--- ajax/script.sh
+++
@@ -1,5 +0,0 @@
-curl "http://portal.cnih.cnh.gob.mx/frameworks/uikit-2.27.1/css/uikit.min.css" > uikit.min.css
-curl "http://portal.cnih.cnh.gob.mx/frameworks/uikit-2.27.1/css/components/sticky.min.css" > sticky.min.css;
-curl "http://portal.cnih.cnh.gob.mx/frameworks/uikit-2.27.1/css/components/notify.min.css" > notify.min.css;
-curl https://js.arcgis.com/4.3/esri/css/main.css > main.css
-
