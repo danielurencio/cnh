d5eb70c4ba1c69b99d9092a8e6980f8766000638
danielurencio <danielurencio@gmail.com>
2017-08-24 19:21:04 -0500

    hola

---
+++ RONDAS/bloques_nuevo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+import pandas as pd
+import numpy as np
+
+f_ = pd.ExcelFile("Licitaciones_ContratosV2.xlsx")
+sheets = f_.sheet_names
+localizacion = f_.parse(sheets[0],skiprows=3)
+caracteristicas = f_.parse(sheets[1],skiprows=3,skipfooter=2)
+#perfiles = f_.parse(sheets[4],skiprows=4)
+
+localizacion.set_index("ID",inplace=True)
+caracteristicas.set_index("ID",inplace=True)
+
+localizacion.drop(["RONDA","LIC"],inplace=True,axis=1)
+
+bloques = localizacion.join(caracteristicas)
+bloques.reset_index(inplace=True)
+bloques.drop('Unnamed: 13',axis=1,inplace=True)
+
+cols = bloques.columns.tolist()
+
+for i in cols:
+  bloques[i] = bloques[i].map(lambda x: x.upper() if isinstance(x,unicode) and not pd.isnull(x) else x)
+
+bloques["API"].replace("--",np.NaN,inplace=True)
+bloques["PROF_PROM"].replace("-",np.NaN,inplace=True)
+
+bloques = bloques[['ID','AREA','NUM','PROV_GEO','CUENCA','ESTADO','SUPERFICIE','UBICACION ','RONDA','LIC','PLAYS','LITOLOGIA','COB_SIS','HCBRO','API','TIRANTE_PROM','TIRANTE_MAX','TIRANTE_MIN','PROF_PROM','CAMPOS']]
+
+bloques.set_index("ID",inplace=True)
+
+bloques.to_csv("DATOS_LICITACIONES_bloques_nuevo.csv", header=None,encoding="latin1")
